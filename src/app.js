b2c298d631e2ea2d3309b52519a6c376e9a5ea1c
knhwong <knhwong123@gmail.com>
2025-10-25 23:46:22 +0100

    Setup DB

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const pool = require('./db/db');
+require('dotenv').config();
+
 const app = express()
-const port = 3000
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(process.env.PGPASSWORD)
+  pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Database connected! Current time:', res.rows[0].now);
+  }
+});
+});
